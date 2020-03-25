cd3921b40394d2656aced24e22508d4fc60059bf
Cory Rosser <coryrosser@gmail.com>
2020-03-25 01:36:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,7 @@ def apply_coupons(cart, coupons)
       else
         item_w_coupon = {
           :item => c_item,
-          :price => coupons[i][:cost] / coupons[:counter][:num],
+          :price => coupons[i][:cost] / coupons[i][:num],
           :clearance => item[:clearance],
           :count => coupons[i][:num]
         }
