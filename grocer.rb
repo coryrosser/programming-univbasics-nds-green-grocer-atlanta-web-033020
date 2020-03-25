d11387aa10e52da45f850f21a5660bf78827a348
Cory Rosser <coryrosser@gmail.com>
2020-03-25 01:05:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,7 +36,17 @@ def apply_coupons(cart, coupons)
   i = 0 
   while i < coupons.length do 
     item = find_item_by_name_in_collection(coupons[i][:item], cart)
+    item_w_coupon = find_item_by_name_in_collection(c_item, cart)
     c_item = "#{coupons[i][:item]} W/ COUPON"
+    if item && c_item[:count] >= coupons[i][:num]
+      if item_w_coupon
+        item_w_coupon[:count] += coupons[i][:num]
+        item[:count] -= coupons[i][:num]
+      else
+        item_w_coupon = {
+          :item => c_item,
+          :price => coupons[i][:cost]
+        }
 end
 
 def apply_clearance(cart)
